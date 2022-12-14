7a69e237d3757d138132253326c31878db239741
Anshu singh <idontknowanything144@gmail.com>
2022-12-14 18:15:17 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,22 @@
+let string = "";
+let buttons = document.querySelectorAll(".btn");
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == "="){
+        string =eval(string);
+        document.querySelector('input').value =string ;
+    }else if(e.target.innerHTML == "C"){
+        string ="";
+        document.querySelector('input').value =string ;
+
+    }else if(e.target.innerHTML == "M+" || e.target.innerHTML == "M-" ){
+    string ="stawp u r too cute for this command";
+        document.querySelector('input').value =string ;
+    }else{
+console.log(e.target);
+string =string + e.target.innerHTML;
+document.querySelector('input').value =string ;}
+
+  })
+  
+})
